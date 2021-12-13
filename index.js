2066da770d069eb22551d3dfd35c48259b6d11ca
TANGW197 <curry.x.tang.-nd@disney.com>
2021-12-13 21:28:52 +0800

    feat: update help argv

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+if (argv['h'] || argv['help']) {
+  console.log(`
+Usage:
+  $ yield                 Select from a list of available generators
+  $ yield <name>          Run a generator registered under that name
+  $ yield <name> [input]  Run the generator with input data to bypass prompts
+
+Options:
+  -h, --help             Show this help display
+  -v, --version          Print current version
+  `);
+  process.exit(0);
+}
 
 Plop.prepare({
   cwd: argv.cwd,
